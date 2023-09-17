c399adc70fec2dabb305d10b6ed871024d043d33
Robert Gerus <ar@is-a.cat>
2023-09-18 00:21:05 +0200

    nixos/scylla: move bird2 config to a separate file.

--- nixos/scylla/configuration.nix
+++ nixos/scylla/configuration.nix
@@ -11,6 +11,14 @@ let
         jq . "$valuePath" | \
           sed -r -e 's@["]__keaInclude ([^"]+)["]@<?include "\1"?>@g'> $out
       '') { };
+
+  dn42_roa_update = pkgs.writeShellScriptBin "dn42_roa_update" ''
+    mkdir -p /etc/bird/
+    ${pkgs.curl}/bin/curl -sfSLR {-o,-z}/etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf
+    ${pkgs.curl}/bin/curl -sfSLR {-o,-z}/etc/bird/roa_dn42.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf
+    ${pkgs.bird2}/bin/birdc c 
+    ${pkgs.bird2}/bin/birdc reload in all
+  '';
 in {
   imports = [ ./hardware-configuration.nix ];
 
@@ -398,144 +406,29 @@ in {
   services.bird2 = {
     enable = true;
     checkConfig = false;
-    config = ''
-        define OWNAS = 4242423137;
-        define OWNIP = 172.20.148.161;
-        define OWNIPv6 = fd25:af2d:1f51:255::1;
-        define OWNNET = 172.20.148.160/27;
-        define OWNNETv6 = fdc0:b038:c31e::/48;
-        define OWNNETSET = [ 172.20.148.160/27+ ];
-        define OWNNETSETv6 = [ fdc0:b038:c31e::/48+ ];
+    config = builtins.readFile ./bird/bird2.conf;
+  };
+  environment.etc."bird/peers/w1kl4s.conf" = { source = ./bird/peers_w1kl4s.conf; };
+  systemd.timers.dn42-roa = {
+    description = "Trigger a ROA table update";
 
-        router id OWNIP;
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitInactiveSec = "1h";
+      Unit = "dn42-roa.service";
+    };
 
-        protocol device {
-            scan time 10;
-        }
-        
-        /*
-         *  Utility functions
-         */
-        
-        function is_self_net() {
-          return net ~ OWNNETSET;
-        }
-        
-        function is_self_net_v6() {
-          return net ~ OWNNETSETv6;
-        }
-        
-        function is_valid_network() {
-          return net ~ [
-            172.20.0.0/14{21,29}, # dn42
-            172.20.0.0/24{28,32}, # dn42 Anycast
-            172.21.0.0/24{28,32}, # dn42 Anycast
-            172.22.0.0/24{28,32}, # dn42 Anycast
-            172.23.0.0/24{28,32}, # dn42 Anycast
-            172.31.0.0/16+,       # ChaosVPN
-            10.100.0.0/14+,       # ChaosVPN
-            10.127.0.0/16{16,32}, # neonetwork
-            10.0.0.0/8{15,24}     # Freifunk.net
-          ];
-        }
-        
-        roa4 table dn42_roa;
-        roa6 table dn42_roa_v6;
-        
-        protocol static {
-            roa4 { table dn42_roa; };
-            include "/etc/bird/roa_dn42.conf";
-        };
-        
-        protocol static {
-            roa6 { table dn42_roa_v6; };
-            include "/etc/bird/roa_dn42_v6.conf";
-        };
-        
-        function is_valid_network_v6() {
-          return net ~ [
-            fd00::/8{44,64} # ULA address space as per RFC 4193
-          ];
-        }
-        
-        protocol kernel {
-            scan time 20;
-        
-            ipv6 {
-                import none;
-                export filter {
-                    if source = RTS_STATIC then reject;
-                    krt_prefsrc = OWNIPv6;
-                    accept;
-                };
-            };
-        };
-        
-        protocol kernel {
-            scan time 20;
-        
-            ipv4 {
-                import none;
-                export filter {
-                    if source = RTS_STATIC then reject;
-                    krt_prefsrc = OWNIP;
-                    accept;
-                };
-            };
-        }
-        
-        protocol static {
-            route OWNNET reject;
-        
-            ipv4 {
-                import all;
-                export none;
-            };
-        }
-        
-        protocol static {
-            route OWNNETv6 reject;
-        
-            ipv6 {
-                import all;
-                export none;
-            };
-        }
-        
-        template bgp dnpeers {
-            local as OWNAS;
-            path metric 1;
-        
-            ipv4 {
-                import filter {
-                  if is_valid_network() && !is_self_net() then {
-                    if (roa_check(dn42_roa, net, bgp_path.last) != ROA_VALID) then {
-                      print "[dn42] ROA check failed for ", net, " ASN ", bgp_path.last;
-                      reject;
-                    } else accept;
-                  } else reject;
-                };
-        
-                export filter { if is_valid_network() && source ~ [RTS_STATIC, RTS_BGP] then accept; else reject; };
-                import limit 1000 action block;
-            };
-        
-            ipv6 {   
-                import filter {
-                  if is_valid_network_v6() && !is_self_net_v6() then {
-                    if (roa_check(dn42_roa_v6, net, bgp_path.last) != ROA_VALID) then {
-                      print "[dn42] ROA check failed for ", net, " ASN ", bgp_path.last;
-                      reject;
-                    } else accept;
-                  } else reject;
-                };
-                export filter { if is_valid_network_v6() && source ~ [RTS_STATIC, RTS_BGP] then accept; else reject; };
-                import limit 1000 action block; 
-            };
-        }
+    wantedBy = [ "timers.target" ];
+    before = [ "bird.service" ];
+  };
 
-      include "/etc/bird/peers/*";
-    '';
+  systemd.services = {
+    dn42-roa = {
+      after = [ "network.target" ];
+      description = "DN42 ROA Updated";
+      unitConfig = { Type = "one-shot"; };
+      serviceConfig = { ExecStart = "${dn42_roa_update}/bin/dn42_roa_update"; };
+    };
   };
 
   security.polkit.enable = true;
