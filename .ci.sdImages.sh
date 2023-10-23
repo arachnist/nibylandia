bc33e204678db8a5715e48ecd53fd4581708d1c3
Robert Gerus <ar@is-a.cat>
2023-10-23 10:58:05 +0200

    ci: build sd images for aarch64 machines

--- .ci.sdImages.sh
+++ .ci.sdImages.sh
@@ -6,11 +6,11 @@ set -x
 
 while read hostOutput; do
   echo "${hostOutput}"
-  nixos-rebuild build --verbose --flake ".#${hostOutput}"
+  nix build --no-link ".#nixosConfigurations.${hostOutput}.config.system.build.sdImage"
 done < <(nix eval -I nixpkgs=$(nix flake metadata nixpkgs --json | jq -r .path) --raw --impure --expr '
     with import <nixpkgs> { };
     (lib.strings.concatStringsSep "\n"
     (lib.mapAttrsToList (n: v: n)
-      (lib.attrsets.filterAttrs (n: v: v.pkgs.system == pkgs.system)
+      (lib.attrsets.filterAttrs (n: v: v.pkgs.system == pkgs.system && v.pkgs.system == "aarch64-linux" && n != builtins.getEnv "HOSTNAME")
         (builtins.getFlake(builtins.toString ./.)).outputs.nixosConfigurations)))
-'; echo "")
+')
