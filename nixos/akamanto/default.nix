4ca8954978c87ac3f80ceb70ac951de40ed2332a
Robert Gerus <ar@is-a.cat>
2023-10-27 09:11:08 +0200

    akamanto: import klipper conifg into Nix

--- nixos/akamanto/default.nix
+++ nixos/akamanto/default.nix
@@ -62,6 +62,8 @@ in {
   users.users.ar.openssh.authorizedKeys.keys =
     config.users.users.root.openssh.authorizedKeys.keys;
   users.users.ar.extraGroups = [ "video" "dialout" "plugdev" ];
+  users.users.root.hashedPassword =
+    "$y$j9T$MO1vp6hFuWqWDJCxyzR.E0$7jJDzeL.fMg64tzmW76HjBy49LC4rtTBHH3/ivGOGc.";
   users.mutableUsers = false;
 
   #environment.persistence."/persistent" = {
@@ -91,7 +93,7 @@ in {
   # strictly printer stuff below
   services.klipper = {
     enable = true;
-    mutableConfig = true;
+    mutableConfig = false;
     firmwares = {
       mcu = {
         enableKlipperFlash = true;
@@ -105,12 +107,154 @@ in {
     };
     settings = {
       mcu.serial = "/dev/ttyACM0";
+      pause_resume = { };
+      display_status = { };
+      virtual_sdcard = { path = "/var/lib/moonraker/gcodes"; };
+      force_move = { enable_force_move = true; };
       printer = {
         kinematics = "corexy";
         max_velocity = 300;
         max_accel = 2000;
         max_z_velocity = 5;
         max_z_accel = 100;
+      };
+
+      fan = { pin = "P2.4"; };
+
+      "fan_generic exhaust" = { pin = "P2.6"; };
+
+      stepper_x = {
+        step_pin = "P2.3";
+        dir_pin = "!P0.22";
+        enable_pin = "!P0.21";
+        microsteps = "32";
+        rotation_distance = "40";
+        endstop_pin = "^P1.24";
+        position_endstop = "220";
+        position_min = "-15";
+        position_max = "220";
+        homing_speed = "80";
+        homing_positive_dir = true;
+      };
+      stepper_y = {
+        step_pin = "P2.2";
+        dir_pin = "!P0.20";
+        enable_pin = "!P0.19";
+        microsteps = "32";
+        rotation_distance = "40";
+        endstop_pin = "^P1.26";
+        position_endstop = "215";
+        position_max = "215";
+        homing_speed = "80";
+        homing_positive_dir = true;
+      };
+      stepper_z = {
+        step_pin = "P2.8";
+        dir_pin = "P2.13";
+        enable_pin = "!P4.29";
+        microsteps = "32";
+        rotation_distance = "4";
+        endstop_pin = "^P1.29";
+        position_endstop = "235";
+        position_min = "-5";
+        position_max = "240";
+        homing_speed = "50";
+        homing_positive_dir = true;
+      };
+
+      extruder = {
+        step_pin = "P2.0";
+        dir_pin = "P0.5";
+        enable_pin = "!P0.4";
+        microsteps = "32";
+        rotation_distance = "8.07";
+        nozzle_diameter = "0.400";
+        filament_diameter = "1.750";
+        heater_pin = "P2.7";
+        sensor_type = "ATC Semitec 104GT-2";
+        sensor_pin = "P0.23";
+        control = "pid";
+        pid_Kp = "38.5";
+        pid_Ki = "2.318";
+        pid_Kd = "160";
+        min_temp = "0";
+        max_temp = "295";
+      };
+      extruder1 = {
+        step_pin = "P2.1";
+        dir_pin = "!P0.11";
+        enable_pin = "!P0.10";
+        microsteps = "32";
+        rotation_distance = "8.07";
+        nozzle_diameter = "0.400";
+        filament_diameter = "1.750";
+        heater_pin = "P1.23";
+        sensor_type = "ATC Semitec 104GT-2";
+        sensor_pin = "P0.24";
+        control = "pid";
+        pid_Kp = "38.5";
+        pid_Ki = "2.318";
+        pid_Kd = "160";
+        min_temp = "0";
+        max_temp = "265";
+      };
+      heater_bed = {
+        heater_pin = "P2.5";
+        sensor_type = "Honeywell 100K 135-104LAG-J01";
+        sensor_pin = "P0.25";
+        control = "watermark";
+        min_temp = "0";
+        max_temp = "130";
+      };
+
+      "gcode_macro CANCEL_PRINT" = {
+        description = "Cancel the actual running print";
+        rename_existing = "CANCEL_PRINT_BASE";
+        gcode = [ "TURN_OFF_HEATERS" "CANCEL_PRINT_BASE" ];
+      };
+      "gcode_macro T0" = {
+        description = "change to tool 0 (high-temp)";
+        gcode = [
+          "SET_GCODE_OFFSET X=0 Y=0 Z=0 MOVE=1"
+          "SAVE_GCODE_STATE"
+          "G91"
+          "G1 Z5"
+          "G90"
+          "G1 X220 Y15 F10000"
+          "RESTORE_GCODE_STATE MOVE=1 MOVE_SPEED=100"
+          "SET_GCODE_OFFSET X=0 Y=0 Z=0"
+        ];
+      };
+      "gcode_macro T1" = {
+        description = "change to tool 1 (normal)";
+        gcode = [
+          "SET_GCODE_OFFSET X=27.45 Y=-0.15 Z=1.6 MOVE=1"
+          "SAVE_GCODE_STATE"
+          "G91"
+          "G1 Z5"
+          "G90"
+          "# X-10 - X27.45"
+          "G1 X-37.45 Y15 F10000"
+          "RESTORE_GCODE_STATE MOVE=1 MOVE_SPEED=100"
+        ];
+      };
+      "gcode_macro TC_DEMO" = {
+        gcode = [
+          "G90"
+          "G1 X100 Y100 Z20 F6000"
+          "T1"
+          "T0"
+          "T1"
+          "T0"
+          "T1"
+          "T0"
+          "T1"
+          "T0"
+          "T1"
+          "T0"
+          "T1"
+          "T0"
+        ];
       };
     };
   };
@@ -158,7 +302,11 @@ in {
     enable = true;
     user = "ar";
     program = "${cageScript}/bin/klipperCageScript";
-    environment = { GDK_BACKEND = "wayland"; };
+    environment = {
+      GDK_BACKEND = "wayland";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    };
+    extraArguments = [ "-d" ];
   };
   systemd.services."cage-tty1".serviceConfig.Restart = "always";
 }
