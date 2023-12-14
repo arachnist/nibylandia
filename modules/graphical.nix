5ddf6715f2602bab0e82eb33b21456faaaaabe19
Robert Gerus <ar@is-a.cat>
2023-12-14 13:29:21 +0100

    m/graphical: prettier boot

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -87,6 +87,21 @@ in {
     libinput.enable = true;
   };
 
+  services.kmscon = {
+    enable = true;
+    hwRender = true;
+  };
+
+  boot = {
+    loader.timeout = 0;
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    initrd.systemd.enable = true;
+    plymouth.enable = true;
+    plymouth.theme = "breeze";
+    kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
+  };
+
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
