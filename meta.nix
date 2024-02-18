1c0a30e532bdc5fd588dd2dac0d2d18707ead970
Robert Gerus <ar@is-a.cat>
2024-02-18 13:43:54 +0100

    meta: avoid repetition with targetHost

--- meta.nix
+++ meta.nix
@@ -3,9 +3,13 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfIRe1nH6vwjQTjqHNnkKAdr1VYqGEeQnqInmf3A6UN ar@khas";
   ar_microlith =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6rEwERSm/Fj4KO4SxFIo0BUvi9YNyf8PSL1FteMcMt ar@microlith";
+  defaultDomain = "tail412c1.ts.net";
 in {
   hosts = builtins.mapAttrs (name: value:
-    builtins.fromJSON (builtins.readFile (./nixos/. + "/${name}/meta.json")))
+    {
+      targetHost = name + "." + defaultDomain;
+    }
+    // builtins.fromJSON (builtins.readFile (./nixos/. + "/${name}/meta.json")))
     (builtins.readDir ./nixos);
 
   users.ar = [ ar_khas ar_microlith ];
