162f828a15e22bb812eb0f4493d9b137b0c3a2aa
Ari Gerus <ar@is-a.cat>
2024-02-20 13:22:27 +0100

    n/microlith: extra steam library space

--- nixos/microlith/hardware-configuration.nix
+++ nixos/microlith/hardware-configuration.nix
@@ -13,6 +13,11 @@
     fsType = "xfs";
   };
 
+  fileSystems."/steam" = {
+    device = "/dev/disk/by-uuid/a2b3af5e-b15b-4023-8f8f-ea828b8df241";
+    fsType = "xfs";
+  };
+
   boot.initrd.luks.devices."microlith".device =
     "/dev/disk/by-uuid/3b53f78f-4d3f-4b3b-b7c8-640fe450f122";
 
