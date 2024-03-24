4f4c80f77452244900fe2fbcdcc24a753d696fe6
Robert Gerus <ar@is-a.cat>
2024-03-24 13:14:22 +0100

    pkgs: minecraft overviewer: update
    drive-by slight rename to make syntax highlighting in Kate happier

--- pkgs/minecraft-overviewer.nix
+++ pkgs/minecraft-overviewer.nix
@@ -1,20 +1,34 @@
-{ fetchFromGitHub, buildPythonPackage, python3Packages, python3, ... }:
+{ fetchFromGitHub, pkgs, buildPythonPackage, python3Packages, python3, ... }:
 
 buildPythonPackage rec {
   pname = "Minecraft-Overviewer";
-  version = "2021-12-14";
+  version = "2024-03-15";
   format = "other";
 
-  propagatedBuildInputs = with python3Packages; [
-    pillow-with-headers
+  propagatedBuildInputs = with pkgs; [
+      pipreqs
+    ] ++ (with python3Packages; [
+    pillow_with_headers
+    altgraph
+    certifi
+    charset-normalizer
+    docopt
+    idna
+    importlib-metadata
+    nbtlib
     numpy
-    networkx
-  ];
+    packaging
+    pefile
+    requests
+    urllib3
+    yarg
+    zipp
+  ]);
 
   buildInputs = with python3Packages; [ setuptools ];
 
   buildPhase = ''
-    export CFLAGS="-I${python3Packages.pillow-with-headers}/include/libImaging"
+    export CFLAGS="-I${python3Packages.pillow_with_headers}/include/libImaging"
     ${python3.interpreter} setup.py build
   '';
 
@@ -23,9 +37,9 @@ buildPythonPackage rec {
   '';
 
   src = fetchFromGitHub {
-    owner = "overviewer";
-    repo = pname;
-    rev = "7171af587399fee9140eb83fb9b066acd251f57a";
-    sha256 = "sha256-iJv4mL1Zr6clL5iuUg1kHoIk9Kk3R4TOYsrldEVyfVo=";
+    owner = "GregoryAM-SP";
+    repo = "The-Minecraft-Overviewer";
+    rev = "4deb15d2cfbaaff7327a39b1e24d03eb4f7878ec";
+    sha256 = "sha256-8YCZ7pk0Rj7wAT5DqGZmNsSI5qQWx5By+1G73yUsAQw=";
   };
 }
