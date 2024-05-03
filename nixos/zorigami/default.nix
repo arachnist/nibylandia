10d7b49f3a4a8a9e2d2b210a1a10fdaea57d9178
Ari Gerus <ar@is-a.cat>
2024-05-03 16:07:13 +0200

    n/zorigami: <Crendgrim> @ari hi, how do I get java 21 on zorigami? Minecraft 1.20.6 requires that to run now

--- nixos/zorigami/default.nix
+++ nixos/zorigami/default.nix
@@ -509,7 +509,7 @@
 
   programs.java = {
     enable = true;
-    package = pkgs.openjdk17;
+    package = pkgs.openjdk21;
   };
 
   environment.systemPackages = with pkgs; [ john restic weechat ];
