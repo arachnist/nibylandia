3f5a949ce4855e1e4f991d5b7ec2ef39163f6168
Ari Gato <ar@is-a.cat>
2024-07-08 21:45:10 +0200

    secrets,meta: add new (mine) key

--- meta.nix
+++ meta.nix
@@ -3,6 +3,8 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfIRe1nH6vwjQTjqHNnkKAdr1VYqGEeQnqInmf3A6UN ar@khas";
   ar_microlith =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6rEwERSm/Fj4KO4SxFIo0BUvi9YNyf8PSL1FteMcMt ar@microlith";
+  ar_kyorinrin =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8Gk/5iqVco5VQLrAJ2Xd7LvgOmr4f5DpQtQHj1DFqi ar@kyorinrin";
   defaultDomain = "tail412c1.ts.net";
 in {
   hosts = builtins.mapAttrs (name: value:
@@ -12,5 +14,5 @@ in {
     // builtins.fromJSON (builtins.readFile (./nixos/. + "/${name}/meta.json")))
     (builtins.readDir ./nixos);
 
-  users.ar = [ ar_khas ar_microlith ];
+  users.ar = [ ar_khas ar_microlith ar_kyorinrin ];
 }
