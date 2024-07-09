e8cd421d7acfcfc56da2ded1c108ef58c151c3a8
Ari Gato <ari@is-a.cat>
2024-07-09 22:01:01 +0200

    n/kyorinrin: coerced secureboot to work, won't need this

--- nixos/kyorinrin/default.nix
+++ nixos/kyorinrin/default.nix
@@ -15,10 +15,6 @@ in
   ];
 
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
-  # boot.initrd.systemd.emergencyAccess = true;
-  # boot.initrd.kernelModules = [ "uhid" "hid_sensor_als" "hid_sensor_trigger" "industrialio_triggered_buffer" "hid_sensor_iio_common" "industrialio" "hid_sensor_hub" "hid_multitouch" "i2c_hid_acpi" "i2c_hid" "mac_hid" "hid_generic" "usbhid" "hid" ];
-  boot.initrd.unl0kr.enable = false;
-  boot.plymouth.enable = true;
 
   services.fprintd.enable = true;
 
