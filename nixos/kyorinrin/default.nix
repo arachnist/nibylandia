1d896dcf78e1a9093c58f27b6fab8e72f1fb15f0
Ari Gato <ar@is-a.cat>
2025-01-24 13:55:11 +0100

    n/kyorinrin: remove 38c3 wifi

--- nixos/kyorinrin/default.nix
+++ nixos/kyorinrin/default.nix
@@ -60,41 +60,6 @@
 
   hardware.graphics.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
 
-  networking.networkmanager.ensureProfiles.profiles = {
-    "38C3" = {
-      connection = {
-        id = "38C3";
-        type = "wifi";
-      };
-      wifi = {
-        mode = "infrastructure";
-        ssid = "38C3";
-      };
-      wifi-security = {
-        auth-alg = "open";
-        key-mgmt = "wpa-eap";
-      };
-      "802-1x" = {
-        anonymous-identity = "38C3";
-        eap = "ttls;";
-        identity = "allowany";
-        password = "allowany";
-        phase2-auth = "pap";
-        altsubject-matches = "DNS:radius.c3noc.net";
-        ca-cert = "${builtins.fetchurl {
-          url = "https://letsencrypt.org/certs/isrgrootx1.pem";
-          sha256 =
-            "sha256:1la36n2f31j9s03v847ig6ny9lr875q3g7smnq33dcsmf2i5gd92";
-        }}";
-      };
-      ipv4 = { method = "auto"; };
-      ipv6 = {
-        addr-gen-mode = "stable-privacy";
-        method = "auto";
-      };
-    };
-  };
-
   systemd.tmpfiles.rules = let
     rocmEnv = pkgs.symlinkJoin {
       name = "rocm-combined";
