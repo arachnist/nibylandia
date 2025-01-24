ceec9ba31a31f56621b83260ffe657e625fe7e7f
Ari Gato <ar@is-a.cat>
2025-01-24 13:54:06 +0100

    n/homekitty: remove rocm; not applicable on intel igpu

--- nixos/homekitty/default.nix
+++ nixos/homekitty/default.nix
@@ -32,8 +32,6 @@
     iio-sensor-proxy
     xournalpp
 
-    # pkgs.comfyuiPackages.rocm.comfyui-with-extensions
-
     woeusb-ng
     ntfs3g
 
@@ -110,11 +108,4 @@
       "networkmanager"
     ];
   };
-
-  systemd.tmpfiles.rules = let
-    rocmEnv = pkgs.symlinkJoin {
-      name = "rocm-combined";
-      paths = with pkgs.rocmPackages; [ rocblas hipblas clr ];
-    };
-  in [ "L+    /opt/rocm   -    -    -     -    ${rocmEnv}" ];
 }
