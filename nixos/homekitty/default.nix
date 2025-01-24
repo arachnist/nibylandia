2315098ab5f8761b2c9769607609bfbad96fe9c2
Ari Gato <ar@is-a.cat>
2025-01-24 13:59:52 +0100

    n/homekitty: remove 38c3 wifi (here as well)

--- nixos/homekitty/default.nix
+++ nixos/homekitty/default.nix
@@ -48,41 +48,6 @@
     libtool
   ];
 
-  networking.networkmanager.ensureProfiles.profiles = {
-    "38C3" = {
-      connection = {
-        id = "38C3";
-        type = "wifi";
-      };
-      wifi = {
-        mode = "infrastructure";
-        ssid = "38C3";
-      };
-      wifi-security = {
-        auth-alg = "open";
-        key-mgmt = "wpa-eap";
-      };
-      "802-1x" = {
-        anonymous-identity = "38C3";
-        eap = "ttls;";
-        identity = "allowany";
-        password = "allowany";
-        phase2-auth = "pap";
-        altsubject-matches = "DNS:radius.c3noc.net";
-        ca-cert = "${builtins.fetchurl {
-          url = "https://letsencrypt.org/certs/isrgrootx1.pem";
-          sha256 =
-            "sha256:1la36n2f31j9s03v847ig6ny9lr875q3g7smnq33dcsmf2i5gd92";
-        }}";
-      };
-      ipv4 = { method = "auto"; };
-      ipv6 = {
-        addr-gen-mode = "stable-privacy";
-        method = "auto";
-      };
-    };
-  };
-
   users.mutableUsers = lib.mkForce true;
 
   users.groups.miau = { gid = 1001; };
