55d4ab647eba97856695064aca2765154e1e974a
Ari Gato <ari@is-a.cat>
2025-04-07 12:09:39 +0200

    p/notbot-rs: version bump

--- pkgs/notbot-rs.nix
+++ pkgs/notbot-rs.nix
@@ -1,7 +1,7 @@
 { fetchFromGitea, rustPlatform, pkg-config, openssl, sqlite, ... }:
 
 let
-  version = "0.1.3";
+  version = "0.1.4";
 in
 rustPlatform.buildRustPackage {
   pname = "notbot-rs";
@@ -12,11 +12,11 @@ rustPlatform.buildRustPackage {
     owner = "ar";
     repo = "notbot";
     tag = "v${version}";
-    hash = "sha256-rRyhyG2UMHiNSewr7DJQPG+jsS3CioF3zxAiwRB4Lg0=";
+    hash = "sha256-NQjHUPrd255VdMEaqzwMKji85603cWw2nir61VZUsdI=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-8yzEN8kiTCo3m8SppX7dmEiSwq3y7PlErwHjPyn9WLQ=";
+  cargoHash = "sha256-vSCh3ngwxe2u7Ihpofy2rl8yG37VrWqAcGfg44tf/wc=";
 
   nativeBuildInputs = [
     pkg-config
